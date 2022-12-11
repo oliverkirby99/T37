a8290891b76face0c0dc30f291b075d3cec98fec
oliverkirby99 <119862042+oliverkirby99@users.noreply.github.com>
2022-12-11 22:42:55 +0000

    Add files via upload

--- garden.py
+++ garden.py
@@ -7,18 +7,33 @@ sentence3 = nlp("Since Jay always jogs a mile seems like a short distance")
 sentence4 = nlp("I convinced her children are noisy")
 sentence5 = nlp("The man who whistles tunes pianos")
 
+
+
 gardenpathSentences = [sentence1, sentence2, sentence3, sentence4, sentence5]
 
 for sentence in gardenpathSentences:
-    print(f"{sentence}\n{[(w.text, w.pos_) for w in sentence]}")
+
+    # Print original sentence:
+    print(sentence)
+
+    # SPLIT THE SENTENCE INTO UNIQUE ELEMENTS
+    [token.orth_ for token in sentence]
+
+    # PRINT THE TOKENIZED SENTENCES
+    print(f"Sentence 'Tokenized")
+    print([(token, token.orth_, token.orth) for token in sentence])  # For each element in the sentence
     print()
+
+    # PRINT THE ENTITY RECOGNITION FOR EACH SENTENCE
+    print(f"Sentence 'entity recognition'")
+    print([(i, i.label_, i.label) for i in sentence.ents])  # Search for recognised elements such as people, events and places
+    print()
+
 input()
 
+
 """
-In the first example, "man" should be a verb because 'The elderly, man the boat' is what is meant here.
+In the sentences I chose, it only found 1 'entity recognition' which was "Jay". It detected this as a 'person'.
 
-In the second sentence about the horse, it seems that the barn fell, however it means the horse (that raced past the barn) fell.
-
-I expected some of the 'pos tags' to be incorrect due to the nature of 'garden-path sentences'.
-But, I spent more time trying to figure out some of the sentences myself :)
+I thought it would possibly detect more entities. But most of my examples included common nouns rather than specific people/places.
 """
